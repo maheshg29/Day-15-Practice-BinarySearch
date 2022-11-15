4f06fa550975b8b76b26b9446d96a5438abd4a94
mahesh <gangurdemahesh9@gmail.com>
2022-11-15 21:10:08 +0530

    [Mahesh]add Binary Search all UC file at a time

---
+++ Day_15_BinarySearch/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_15_BinarySearch
+{
+    public class Node
+    {
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+    }
+}
